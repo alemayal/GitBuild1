8c546f56670fa468098cd19372f07822978e378b
Andy Finley <afinley@rocketsoftware.com>
2020-04-30 01:50:26 -0700

    Task ID: SS-38

--- creditcheck.jsp
+++ creditcheck.jsp
@@ -1,4 +1,4 @@
-<%-- Comment --%>
+<%-- Tax Update --%>
 			
 <%@ taglib uri="http://java.sun.com/portlet" prefix="portlet" %>
 <portlet:defineObjects />
--- custmaint.php
+++ custmaint.php
@@ -5,7 +5,7 @@ foreach($GLOBALS as $arraykey=>$arrayvalue){if($arraykey[0]!='_' && $arraykey!="
 
 //	Program Name:		custmaint.php
 //	Program Title:		Maintain customer master file
-//	Created by:			joebAF
+//	Created by:			ANDYF
 //	Task:	     		ISSUE358
 //	Template family:	Idaho
 //	Template name:		SQL Page at a time maintenance
